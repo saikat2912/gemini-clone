168ae8e8b28776dc839652341dfa8e48dc6af0cb
saikat2912 <saikat2270@gmail.com>
2024-04-12 14:11:40 +0530

    Prompt result , typing effect , text formating added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import './main.css'
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 const Main =()=>{
+    const { prevPrompts,setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResults,
+        isLoading,
+        resultData,
+        setInput,
+        input} =useContext(Context);
+
     return(
         <div>
             <div className="main">
@@ -11,7 +22,11 @@ const Main =()=>{
                     <img src={assets.user_icon} alt=""/>
                 </div>
                 <div className="main-container">
-                    <div className="greet">
+
+                    {console.log("What is show Results ",showResults," results are ",resultData)}
+                    {!showResults?
+                    <>
+                         <div className="greet">
                         <p><span>Hello , Saikat.</span></p>
                         <p>How can I help you today?</p>
                     </div>
@@ -33,14 +48,34 @@ const Main =()=>{
                             <img src={assets.code_icon} alt=""/>
                         </div>
                     </div>
+                    </>:
+                    <div className="result">
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt=""/>
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt=""/>
+                            {isLoading?
+                            <div className="loader">
+                                <hr/>
+                                <hr/>
+                                <hr/>
+                            </div>:<div>
+                            <p dangerouslySetInnerHTML={{__html:resultData}}/></div>}
+                           
+                        </div>
+                    </div>
+                        }
+               
                 </div>
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder="Enter a prompt"/>
+                        <input type="text" placeholder="Enter a prompt" onChange={(e)=>setInput(e.target.value)} value={input}/>
                         <div>
                             <img src={assets.gallery_icon} alt=""/>
                             <img src={assets.mic_icon} alt=""/>
-                            <img src={assets.send_icon} alt=""/>
+                            <img src={assets.send_icon} alt="" onClick={()=>onSent()}/>
                         </div>
                     </div>
                     <p className="bottom-info">Gemini might display wrong coz its just an AI , so employ a human to recheck!!</p>
---
+++ src/context/Context.jsx
@@ -0,0 +1,75 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+export const Context=createContext();
+
+const ContextProvider =(props)=>{
+
+    const [input,setInput] = useState("");
+    const [recentPrompt,setRecentPrompt] = useState("");
+    const [prevPrompts,setPrevPrompts] = useState([]);
+    const [showResults,setShowResults]= useState(false);
+    const [isLoading,setLoading] = useState(false);
+    const [resultData,setResultData] = useState("")
+
+    const delayPara = (index,nextWord)=>{
+        setTimeout(function(){
+            setResultData(prev=>prev+nextWord)
+        },75*index)
+    }
+
+    const onSent = async(prompt)=>{
+
+        setResultData("")
+        setLoading(true)
+        setShowResults(true)
+        setRecentPrompt(input)
+        const response=await runChat(input)
+        let responseArray = response.split("**");
+        let newArray;
+        console.log('Response with out start is ',responseArray)
+        for(let i=0;i<responseArray.length;i++){
+            if(i==0 || i%2!=1){
+                newArray+=responseArray[i];
+            }
+            else{
+                newArray+="<b>"+responseArray[i]+"</b>";
+            }
+
+        }
+        console.log("The first response is ",newArray)
+        let newResponse2 = newArray.split("*").join("</br>");
+        let newResponseArray = newResponse2.split(" ");
+        for(let i=0;i<newResponseArray.length;i++){
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ")
+        }
+       
+       // setResultData(newResponse2);
+        setLoading(false)
+        setInput("");
+
+    }
+
+  
+
+    const contextValue={
+        prevPrompts,setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResults,
+        isLoading,
+        resultData,
+        setInput,
+        input
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider;
